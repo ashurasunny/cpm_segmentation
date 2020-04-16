e125e293d17d68703215bb71816017ad0d85ecdd
ashurasunny <649717518@qq.com>
2020-04-16 12:19:09 +0100

    update test

--- regression_cpm/models/pix2pix_model.py
+++ regression_cpm/models/pix2pix_model.py
@@ -255,16 +255,16 @@ class Pix2PixModel(BaseModel):
     def test(self):
         with torch.no_grad():
             self.fake_M = self.netG_1(self.real_A)
-            self.real_AM = torch.cat((self.real_A, self.real_M), 1)
-            self.fake_B_1 = self.netG_2(self.real_AM)
+            # self.real_AM = torch.cat((self.real_A, self.real_M), 1)
+            # self.fake_B_1 = self.netG_2(self.real_AM)
 
-            fake_M_2 = self.real_A.data.cpu().numpy()
+            # fake_M_2 = self.real_A.data.cpu().numpy()
             fake_M = self.fake_M.data.cpu().numpy()
 
-            fake_M_2[fake_M==0] = 0
+            # fake_M_2[fake_M==0] = 0
 
-            fake_M_2 = torch.from_numpy(fake_M_2).cuda()
+            # fake_M_2 = torch.from_numpy(fake_M_2).cuda()
 
-            self.fake_AM = torch.cat((self.real_A, fake_M_2), 1)
+            self.fake_AM = torch.cat((self.real_A, fake_M), 1)
             self.fake_B_2 = self.netG_2(self.fake_AM)
         return self.fake_M, self.fake_B_1, self.fake_B_2
